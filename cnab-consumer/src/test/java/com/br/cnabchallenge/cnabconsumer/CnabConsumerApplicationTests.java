8d858dcd975320a997be1bf13c2ce137897ac4f2
Gustavo Henrique Pio <pio.ghp@gmail.com>
2023-07-22 16:04:36 -0300

    :wrench: Creating cnab-consumer service and cnab-query service project template.

---
+++ cnab-consumer/src/test/java/com/br/cnabchallenge/cnabconsumer/CnabConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cnabchallenge.cnabconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
